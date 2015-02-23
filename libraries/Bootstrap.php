f9c87ffbf3ca01b5c895b9d22f8b8c21f6405c29
mdmunir <misbahuldmunir@gmail.com>
2015-02-23 11:48:03 +0700

    first commit for angular

---
+++ libraries/Bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace libraries;
+
+use Yii;
+
+/**
+ * Bootstrap
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class Bootstrap implements \yii\base\BootstrapInterface
+{
+    protected $bootstraps = [
+        'biz\\core\\Bootstrap',
+        'dee\\angular\\Bootstrap',
+    ];
+
+    /**
+     * @param \yii\base\Application $app
+     */
+    public function bootstrap($app)
+    {
+        foreach ($this->bootstraps as $bootstrap) {
+            $component = Yii::createObject($bootstrap);
+            if ($component instanceof \yii\base\BootstrapInterface) {
+                $component->bootstrap($app);
+            }
+        }
+    }
+}
---
+++ libraries/easyui/widgets/Combobox.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace dee\easyui\widgets;
+
+/**
+ * Combobox
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class Combobox extends InputWidget
+{
+    
+    public function run()
+    {
+        
+        $this->registerWidget('combobox');
+    }
+}
