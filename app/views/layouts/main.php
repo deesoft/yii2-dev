b1521605c587e58f374b48186e5312f3a97efd8c
mdmunir <misbahuldmunir@gmail.com>
2015-04-16 18:18:56 +0700

    Squashed 'application/' changes from cc756a4..66b50c6
    66b50c6 Update README.md
    673cc2a application
    b5cb949 main layout
    d686f11 composer
    14c8a00 
    5f10154 move repository
    134f7d1 move config to local
    a0b40f9 move `app/comman` to libraries
    8834d56 add feaker
    
    git-subtree-dir: application
    git-subtree-split: 66b50c60d7642fda6992b92c4382952c766e2822

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -1,72 +1,68 @@
 <?php
 
 use yii\helpers\Html;
-use yii\helpers\Inflector;
-use yii\widgets\Breadcrumbs;
+use dee\easyui\EasyuiAsset;
+use dee\easyui\NavTree;
 
-/**
- * @var \yii\web\View $this
- * @var string $content
- */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-$asset = app\assets\AppAsset::register($this);
+EasyuiAsset::register($this);
+app\assets\AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>" manifest="<?= isset($this->context->manifestFile) ? $this->context->manifestFile : '' ?>">
+<html>
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
+        <style>
+            .breadcrumb {
+                background-color: #f5f5f5;
+                border-radius: 4px;
+                list-style: outside none none;
+                margin-bottom: 20px;
+                padding: 8px 15px;
+            }
+        </style>
     </head>
     <?php $this->beginBody() ?>
-    <body class="skin-blue fixed">
-        <header class="header">
-            <?php echo $this->render('heading', ['baseurl' => $asset->baseUrl]); ?>
-        </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">
-            <aside class="right-side">
-                <section class="content-header">
-                    <h1>
-                        <?= '&nbsp;' . Html::encode($this->title) ?>
-                        <small><?php echo \Yii::$app->controller->id . '-' . \Yii::$app->controller->action->id; ?></small>
-                    </h1>
-                    <?php
-                    $breadcrumbs = isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [];
-                    foreach (Yii::$app->controller->modules as $module) {
-                        if ($module != Yii::$app) {
-                            array_unshift($breadcrumbs, ['label' => Inflector::camel2words($module->id), 'url' => ['/' . $module->uniqueId]]);
-                        }
-                    }
-                    ?>
-                    <?=
-                    Breadcrumbs::widget([
-                        'tag'=>'ol',
-                        'encodeLabels'=>false,
-                        'homeLink'=>['label'=>'<i class="fa fa-dashboard"></i> Home/Dashboard','url'=>['/site/index']],
-                        'links' => $breadcrumbs,
-                    ])
-                    ?>
-                </section>
-                <section class="content">
-                    <?= $content ?>
-                </section>
-            </aside>
-            <aside class="left-side sidebar-offcanvas">
-                <?php echo $this->render('sidebar', ['baseurl' => $asset->baseUrl]); ?>
-            </aside>
+    <body class="easyui-layout" style="background: #eee;text-align:left;">
+        <div region="north" border="false" style="background:#3c8dbc;height: 70px;">
+            <h2>
+                <?= Html::a(Yii::$app->name, Yii::$app->homeUrl) ?>
+            </h2>
         </div>
-
-        <!--        <footer class="footer">
-                    <div class="container">
-                        <p class="pull-left">&copy; My Company <?= ''//date('Y')              ?></p>
-                        <p class="pull-right"><?= ''//Yii::powered()              ?></p>
-                    </div>
-                </footer>-->
-    </div>
-    <?php $this->endBody() ?>
-</body>
+        <div region="west" title="Nav" style="width:250px;padding:5px;">
+            <?php
+            $items = [
+                'satu',
+                [
+                    'label' => 'dua',
+                    'items' => [
+                        'dua-satu',
+                        'dua-dua'
+                    ]
+                ],
+                [
+                    'label' => 'tiga',
+                    'url' => ['tiga']
+                ]
+            ];
+            ?>
+            <?=
+            NavTree::widget([
+                'items' => $items,
+            ]);
+            ?>
+        </div>
+        <div region="center" style="padding-left: 20px;padding-top: 10px;">
+            <?= $content; ?>
+        </div>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
--- environments/dev/app/config/console-local.php
+++ environments/dev/app/config/console-local.php
@@ -1,4 +1,17 @@
 <?php
 
 return[
+    'controllerMap' => [
+        'migrate' => [
+            'class' => 'dee\console\MigrateController',
+            'migrationLookup' => [
+                '@yii/rbac/migrations',
+                '@mdm/admin/migrations',
+            ]
+        ],
+        'sample-data' => [
+            'class' => 'dee\console\SampleDataController',
+            'samples' => '@app/command/samples.php'
+        ]
+    ],
 ];
