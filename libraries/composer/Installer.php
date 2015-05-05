6dcfab435eb27cea60c5627ad6f560de4e06d050
mdmunir <misbahuldmunir@gmail.com>
2015-05-05 11:18:32 +0700

    :grinning:

--- libraries/composer/Installer.php
+++ libraries/composer/Installer.php
@@ -2,9 +2,11 @@
 
 namespace dee\composer;
 
+use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 use Composer\Util\Filesystem;
 use Composer\Script\CommandEvent;
+use Composer\Script\ScriptEvents;
 
 /**
  * Configure
@@ -12,9 +14,39 @@ use Composer\Script\CommandEvent;
  * @author Misbahul D Munir <misbahuldmunir@gmail.com>
  * @since 1.0
  */
-class Installer extends \yii\composer\Installer
+class Installer extends LibraryInstaller
 {
+    const EXTRA_BOOTSTRAP = 'bootstrap';
+    const EXTENSION_FILE = 'yiisoft/extensions.php';
+    const EXTRA_SYMLINK = 'symlinks';
+    const EXTRA_PERMISSION = 'permission';
+
     protected $baseDir;
+
+    protected function addPackage(PackageInterface $package, CommandEvent $event = null)
+    {
+        $extension = [
+            'name' => $package->getName(),
+            'version' => $package->getVersion(),
+        ];
+
+        $alias = $this->generateDefaultAlias($package);
+        if (!empty($alias)) {
+            $extension['alias'] = $alias;
+        }
+        $extra = $package->getExtra();
+        if (isset($extra[self::EXTRA_BOOTSTRAP])) {
+            $extension['bootstrap'] = $extra[self::EXTRA_BOOTSTRAP];
+        }
+        $extensions = $this->loadExtensions();
+        $extensions[$package->getName()] = $extension;
+        $this->saveExtensions($extensions);
+
+        if ($event && $event->getName() == ScriptEvents::POST_INSTALL_CMD) {
+            $this->generateSymlink($package);
+            $this->setPermission($package);
+        }
+    }
 
     protected function generateDefaultAlias(PackageInterface $package)
     {
@@ -60,6 +92,35 @@ class Installer extends \yii\composer\Installer
         return $aliases;
     }
 
+    protected function loadExtensions()
+    {
+        $file = $this->vendorDir . '/' . self::EXTENSION_FILE;
+        if (!is_file($file)) {
+            return [];
+        }
+        // invalidate opcache of extensions.php if exists
+        if (function_exists('opcache_invalidate')) {
+            opcache_invalidate($file, true);
+        }
+        $extensions = require($file);
+
+        $vendorDir = str_replace('\\', '/', $this->vendorDir);
+        $n = strlen($vendorDir);
+
+        foreach ($extensions as &$extension) {
+            if (isset($extension['alias'])) {
+                foreach ($extension['alias'] as $alias => $path) {
+                    $path = str_replace('\\', '/', $path);
+                    if (strpos($path . '/', $vendorDir . '/') === 0) {
+                        $extension['alias'][$alias] = '<vendor-dir>' . substr($path, $n);
+                    }
+                }
+            }
+        }
+
+        return $extensions;
+    }
+
     protected function saveExtensions(array $extensions)
     {
         $file = $this->vendorDir . '/' . self::EXTENSION_FILE;
@@ -69,17 +130,17 @@ class Installer extends \yii\composer\Installer
         $baseDir = $this->baseDir;
         if (strpos($this->vendorDir . '/', $baseDir . '/') === 0) {
             $ch = substr($this->vendorDir, strlen($baseDir));
-            if(strpos($ch, '..') === false){
+            if (strpos($ch, '..') === false) {
                 $c = substr_count($ch, '/');
                 $dir = '$vendorDir';
-                for($i=0;$i<$c;$i++){
+                for ($i = 0; $i < $c; $i++) {
                     $dir = "dirname($dir)";
                 }
-            }  else {
-                $dir = var_export($this->vendorDir,true);
+            } else {
+                $dir = var_export($this->vendorDir, true);
             }
-        }  else {
-            $dir = var_export($this->vendorDir,true);
+        } else {
+            $dir = var_export($this->vendorDir, true);
         }
         $array = var_export($extensions, true);
         $array = str_replace("'<vendor-dir>", '$vendorDir . \'', $array);
@@ -99,11 +160,56 @@ FILE;
             opcache_invalidate($file, true);
         }
     }
-    
+
+    protected function generateSymlink(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+        if (isset($extra[self::EXTRA_SYMLINK])) {
+            $fs = new Filesystem;
+            $baseDir = $this->baseDir;
+            foreach ($extra[self::EXTRA_SYMLINK] as $dest => $src) {
+                if (!$fs->isAbsolutePath($dest)) {
+                    $dest = $baseDir . '/' . $dest;
+                }
+                echo "symlink('$src', '$dest')...";
+                if (!is_dir($dest) && !is_file($dest)) {
+                    if (!$fs->isAbsolutePath($src)) {
+                        $src = $baseDir . '/' . $src;
+                    }
+                    symlink($src, $dest);
+                    echo "done.\n";
+                } else {
+                    echo "file destination exists.\n";
+                }
+            }
+        }
+    }
+
+    protected function setPermission(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+        if (isset($extra[self::EXTRA_PERMISSION])) {
+            $fs = new Filesystem;
+            $baseDir = $this->baseDir;
+            foreach ($extra[self::EXTRA_PERMISSION] as $path => $permission) {
+                if (!$fs->isAbsolutePath($path)) {
+                    $path = $baseDir . '/' . $path;
+                }
+                echo "chmod('$path', $permission)...";
+                if (is_dir($path) || is_file($path)) {
+                    chmod($path, octdec($permission));
+                    echo "done.\n";
+                } else {
+                    echo "file not found.\n";
+                }
+            }
+        }
+    }
+
     public static function apply(CommandEvent $event)
     {
         $composer = $event->getComposer();
         $installer = new static($event->getIO(), $composer);
-        $installer->addPackage($composer->getPackage());
+        $installer->addPackage($composer->getPackage(), $event);
     }
 }
