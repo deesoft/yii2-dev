32fa7c5a40b609fd6e64070f64d35769e11c05e3
mdmunir <misbahuldmunir@gmail.com>
2015-08-22 10:45:07 +0700

    remove yii2-admin

--- libraries/admin/views/default/route/form.js
+++
@@ -1,7 +0,0 @@
-$scope.ok = function () {
-    $modalInstance.close($scope.route);
-};
-
-$scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-};
--- libraries/admin/views/default/rule/view.js
+++
@@ -1,9 +0,0 @@
-$sce = $injector.get('$sce');
-
-$scope.name = name;
-Rule.get({
-    id: name,
-    expand: 'content'
-}, function (r) {
-    $scope.content = $sce.trustAsHtml(r.content);
-});
